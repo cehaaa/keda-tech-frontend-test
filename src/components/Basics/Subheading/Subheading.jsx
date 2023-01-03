711cd1bcac292e69481bcbee3b1d5dfb6cc93b0f
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-04 02:30:58 +0700

    style: fix subheading style

--- src/components/Basics/Subheading/Subheading.jsx
+++ src/components/Basics/Subheading/Subheading.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Subheading = ({ children }) => {
-	return <div className='text-blue-600 font-bold'>{children}</div>;
+const Subheading = ({ children, className }) => {
+	return (
+		<div className={`text-blue-600 font-bold mb-2 ${className}`}>
+			{children}
+		</div>
+	);
 };
 
 export default Subheading;
