b4cd42235b47ea9d6d444d64d7e4f379383bda16
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-04 02:33:05 +0700

    style: fix text typography

--- src/components/Basics/Text/Text.jsx
+++ src/components/Basics/Text/Text.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Text = ({ children, className }) => {
-	return <p className={`${className} text-gray-500  `}>{children}</p>;
+	return <p className={`${className}`}>{children}</p>;
 };
 
 export default Text;
