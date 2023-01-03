a373fdc670531b29532084a04fca003d5dfd40ac
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-04 02:33:49 +0700

    style: fix TierCard style

--- src/components/TierCard/TierCard.jsx
+++ src/components/TierCard/TierCard.jsx
@@ -5,23 +5,26 @@ import Button from "../Basics/Button/Button";
 
 const TierCard = ({ tier }) => {
 	return (
-		<div className='hover:bg-blue-50 transform hover:-translate-y-3 duration-200 cursor-pointer'>
+		<div className='md:hover:bg-blue-50 border md:border-none transform md:hover:-translate-y-3 duration-200 cursor-pointer'>
 			<div className='p-5'>
 				<Text className='text-black text-3xl mb-2 font-semibold'>
 					{tier.title}
 				</Text>
-				<Text>Perfect for started</Text>
-				<Button className='w-full my-3'>Choose this plan</Button>
-				<div className='text-3xl my-3 font-bold'>
+				<Text className='text-lg mb-3 text-gray-500'>{tier.description}</Text>
+				<Button className='w-full mb-3'>Choose this plan</Button>
+				<div className='text-3xl mb-5 font-bold'>
 					${tier.price}
 					<span className='text-gray-400 font-semibold'>/mo</span>
 				</div>
-				<Text>(Per subscribe per month)</Text>
 
-				<Text className='mt-3 font-medium text-black'>Benefits</Text>
-				<div className='mt-3 space-y-3'>
+				<Text className='text-xl font-medium text-black mb-3'>Benefits</Text>
+				<div className='space-y-2'>
 					{tier.benefits.map((benefit, index) => {
-						return <Text key={index}>{benefit}</Text>;
+						return (
+							<Text className='text-lg text-gray-500' key={index}>
+								{benefit}
+							</Text>
+						);
 					})}
 				</div>
 			</div>
