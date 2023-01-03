dfc7d3de2f2788d1ac296e86a166ea76af4b9ec6
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-04 02:29:52 +0700

    style: fix hyperlink style

--- src/components/Basics/Hyperlink/Hyperlink.jsx
+++ src/components/Basics/Hyperlink/Hyperlink.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Hyperlink = ({ children, href = "#" }) => {
+const Hyperlink = ({ children, href = "#", className }) => {
 	return (
 		<a
 			href={href}
-			className='text-gray-500 hover:text-blue-600 duration-200 cursor-pointer hover:underline underline-offset-2 font-medium '>
+			className={` duration-200 cursor-pointer hover:underline underline-offset-2`}>
 			{children}
 		</a>
 	);
